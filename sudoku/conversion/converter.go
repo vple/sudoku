2044efd0ed6ec3edb885d17505213ac07d011c9b
Vincent Le <vince1123581321@gmail.com>
2020-11-09 09:56:00 -0500

    Cleanup.

--- sudoku/conversion/converter.go
+++ sudoku/conversion/converter.go
@@ -19,11 +19,11 @@ func ParseState(state map[string]bool) sudoku.Board {
 	return sudoku.NewStandardBoard(initialValues)
 }
 
+// ToFormula converts a board to CNF form.
 func ToFormula(board sudoku.Board) (formula sat.ConjunctiveFormula) {
 	constraints := board.AllConstraints()
 
 	for _, constraint := range constraints {
-		// fmt.Printf("%T: %v\n", constraint, constraint)
 		formula = formula.And(convert(constraint, board))
 	}
 
@@ -45,79 +45,4 @@ func convert(c sudoku.Constraint, board sudoku.Board) sat.ConjunctiveFormula {
 	default:
 		panic(fmt.Sprintf("Unknown constraint type: %T", c))
 	}
-}
-
-// ToFormula returns the formula that defines a board.
-func ToFormula2(b sudoku.Board) sat.ConjunctiveFormula {
-	formula := sat.EmptyConjunctiveFormula()
-
-	// Constraints for individual cells.
-	// clauses := make([]sat.DisjunctiveClause, 0)
-	for _, coordinate := range b.AllCoordinates() {
-		// Coordinate has exactly one value from 1-9.
-		hasOneValue := sat.ExactlyOneTrue(toLiterals(coordinate))
-		formula = formula.And(hasOneValue)
-
-		// clauses = append(clauses, coordinate.Clauses()...)
-
-		if value, ok := b.Value(coordinate); ok {
-			// If value is known, specify it.
-			exactValue := sat.NewDisjunctiveClause(toLiteral(coordinate, value))
-			formula = formula.And(exactValue.ToFormula())
-			// clauses = append(clauses, clause)
-		}
-	}
-	// formula = formula.And(sat.ConjunctiveFormula{clauses})
-
-	// Constraints for rows.
-	for _, row := range b.AllRows() {
-		// Values in each row are unique.
-		formula = formula.And(uniqueValues(row, b.AllValues()))
-		// Each value appears in each row.
-		for value := 1; value <= b.Size(); value++ {
-			formula = formula.And(Appears(row, value))
-		}
-	}
-
-	// Constraints for cols.
-	for _, col := range b.AllCols() {
-		// Values in each col are unique.
-		formula = formula.And(uniqueValues(col, b.AllValues()))
-		// Each value appears in each col.
-		for value := 1; value <= b.Size(); value++ {
-			formula = formula.And(Appears(col, value))
-		}
-	}
-
-	// Constraints for regions.
-	for _, region := range b.AllRegions() {
-		// Values in each region are unique.
-		formula = formula.And(uniqueValues(region, b.AllValues()))
-		// Each value appears in each region.
-		for value := 1; value <= b.Size(); value++ {
-			formula = formula.And(Appears(region, value))
-		}
-	}
-
-	// for i := 1; i <= b.Size(); i++ {
-	// 	row := b.Row(i)
-	// 	col := b.Col(i)
-	// 	// Currently assumes size is 9.
-	// 	region := b.Region((i-1)/3+1, (i-1)%3+1)
-
-	// 	formula = formula.And(UniqueValues(row)).And(Appears(row, allValues...))
-	// 	formula = formula.And(UniqueValues(col)).And(Appears(col, allValues...))
-	// 	formula = formula.And(UniqueValues(region)).And(Appears(region, allValues...))
-	// }
-
-	// Additional rules.
-	// if b.rules.diagonalsUnique {
-	// 	diagonal := b.Diagonal(sudoku.NewCoordinate(1, 1), sudoku.NewCoordinate(9, 9))
-	// 	formula = formula.And(UniqueValues(diagonal)).And(Appears(diagonal, allValues...))
-
-	// 	diagonal = b.Diagonal(NewCoordinate(1, 9), NewCoordinate(9, 1))
-	// 	formula = formula.And(UniqueValues(diagonal)).And(Appears(diagonal, allValues...))
-	// }
-
-	return formula
 }
